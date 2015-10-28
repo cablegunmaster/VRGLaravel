2d26e42f81bdcaeb709edb38322e76ac53543f22
David Visscher <Davidjulianvisscher@hotmail.com>
2015-10-28 11:58:12 +0100

    fixed merge conflicts

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,64 +1,7 @@
 @extends('template')
 @section('title', 'map')
-
-<<<<<<< HEAD:resources/views/index.blade.php
-    <style>
-        .nopadding {
-            padding: 0px 0px 0px 0px !important;
-        }
-        .nomargin {
-            margin: 0px 0px 0px 0px !important;
-        }
-
-        .noBottomMargin {
-            margin-bottom: 0px !important;
-        }
-    </style>
-
-    <script src='https://api.mapbox.com/mapbox.js/v2.2.2/mapbox.js'></script>
-    <link href='https://api.mapbox.com/mapbox.js/v2.2.2/mapbox.css' rel='stylesheet'/>
-
-    <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v0.0.4/Leaflet.fullscreen.min.js'></script>
-    <link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v0.0.4/leaflet.fullscreen.css' rel='stylesheet' />
-
-    <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-minimap/v1.0.0/Control.MiniMap.js'></script>
-    <link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-minimap/v1.0.0/Control.MiniMap.css' rel='stylesheet' />
-
-    <script src='https://api.mapbox.com/mapbox.js/plugins/mapbox-directions.js/v0.4.0/mapbox.directions.js'></script>
-    <link rel='stylesheet' href='https://api.mapbox.com/mapbox.js/plugins/mapbox-directions.js/v0.4.0/mapbox.directions.css' type='text/css' />
-
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
-
-<body class="grey darken-3 white-text">
-<nav id="navbar">
-    <div class="nav-wrapper grey darken-4">
-        <a href="#" class="brand-logo white-text">&nbsp;&nbsp;Meetploeg app</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a class="modal-trigger waves-effect waves-light" href="#teamsModal" onclick="teamsModal_open();">Teams</a></li>
-            <li><a href="#">Geschiedenis</a></li>
-            <li><a href="#">Instellingen</a></li>
-        </ul>
-    </div>
-</nav>
-
-<!-- Modal Area -->
-
-<div id="teamsModal" class="modal modal-fixed-footer">
-    <div class="modal-content black-text" id="teamsModalContent">
-        <p>Loading</p>
-    </div>
-    <div class="modal-footer">
-        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat center">Sluiten</a>
-    </div>
-</div>
-
-<!-- Map Area -->
-
-=======
 @section('content')
->>>>>>> add form and insert to database:resources/views/map.blade.php
+
 <div class="row noBottomMargin">
     <div class="col s3 black-text">
         <div class="card-panel white-text blue darken-2" style="padding-top:5px;padding-bottom:5px">
