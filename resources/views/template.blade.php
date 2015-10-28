f59692c5aca96e02b06e0f6188beae9865506f08
cablegunmaster <jasper.lankhorst@gmail.com>
2015-10-28 15:51:02 +0100

    -HotFix, assets verbeterd ( temp fix removed).

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,7 @@
 	<!--Import Google Icon Font-->
 	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!--Import materialize.css-->
-	<link type="text/css" rel="stylesheet" href="http://scrumbag.nl/brandweer/css/materialize.min.css" media="screen,projection"/>
+	<link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}" media="screen,projection"/>
 
 	<style>
 		.nopadding {
@@ -69,7 +69,7 @@
 
 	<!--Import jQuery before materialize.js-->
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="http://scrumbag.nl/brandweer/js/materialize.min.js"></script>
+	<script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
 	<div class="content_container">
 		@yield('content')
 	</div>
