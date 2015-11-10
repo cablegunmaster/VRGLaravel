22025ea6a83466ecf8168aa811fddc43402f3307
Johan Wouda <johan.wouda@gmail.com, j.m.wouda@st.hanze.nl>
2015-11-10 11:08:58 +0100

    Fixed another typo

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -53,7 +53,7 @@ class LoginController extends Controller
             return json_encode($json);
         }
         $json = array(
-            'succes' => false
+            'success' => false
         );
         return json_encode($json);
     }
