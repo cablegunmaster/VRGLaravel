738ea7fbb9f1365cc136c51fd7c5702c2ec86899
David Visscher <Davidjulianvisscher@hotmail.com>
2015-11-12 01:02:52 +0100

    Teams are now denoted by number on the map

--- resources/views/api/GEOJsonLocation.blade.php
+++ resources/views/api/GEOJsonLocation.blade.php
@@ -2,6 +2,7 @@
     "type": "FeatureCollection",
     "features": [
 @for ($i = 0; $i < count($locations); $i++)
+    
     @if(isset($locations[$i]->lon))
     { "type": "Feature",
         "geometry": {
@@ -16,7 +17,13 @@
         "details": "Some Text",
         "type": "firetruck",
         "marker-color": "{{ "#".substr(hash('md5',$locations[$i]->team_code),0,6)}}",
-        "marker-symbol": "police",
+        <?php 
+            $substr = substr($locations[$i]->team_code, -2); 
+            if($substr[0] == "0"){
+                $substr=substr($substr, -1);
+                } 
+        ?>
+        "marker-symbol": "{{$substr}}",
         "icon": {
             "iconUrl": "/font/police-24.png",
             "iconSize": [35,17],
